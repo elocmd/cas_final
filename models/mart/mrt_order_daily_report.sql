d4d94bcf909914ab338b179f980f3c355b8dc915
Elodie Cormand <elodiecormand@gmail.com>
2024-11-07 08:36:44 +0000

    doc

--- models/mart/mrt_order_daily_report.sql
+++ models/mart/mrt_order_daily_report.sql
@@ -1,12 +1,12 @@
 SELECT DATE_TRUNC(order_created_at, DAY) AS report_date,
-    mapping.account_manager,
-    mapping.state,
+    mapping.account_manager_name,
+    orders.customer_state,
     COUNT(DISTINCT order_id) AS total_orders,
-    AVG(total_distinct_items) AS average_total_distinct_items,
-    SUM(sum_feedback_score) / SUM(total_feedbacks) AS average_feedback_score,
-    AVG(total_order_amount) AS average_total_order_amount
+    coalesce(AVG(total_distinct_order_item),0) AS average_total_distinct_order_item,
+    coalesce(avg(feedback_avg_score),0) AS average_feedback_score,
+    coalesce(AVG(total_order_amount),0) AS average_total_order_amount
 FROM {{ ref('int_sales_database__order') }} AS orders
-LEFT JOIN {{ ref('stg_google_sheets__account_manager_region_mapping') }} as mapping ON orders.user_state = mapping.state
+LEFT JOIN {{ ref('stg_google_sheets__account_manager_region_mapping') }} as mapping ON orders.customer_state = mapping.account_manager_state
 GROUP BY report_date,
-    account_manager,
-    state
+    account_manager_name,
+    customer_state
