e24be27d72417534a2daad3aa2b3154f96513318
Elodie Cormand <elodiecormand@gmail.com>
2024-11-08 09:47:25 +0000

    kk

---
+++ tests/total_order_amount_positive.sql
@@ -0,0 +1,8 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where total_amount < 0 to make the test fail.
+select
+    order_id,
+    sum(total_amount) as total_amount
+from {{ ref('stg_sales_database__order_item') }}
+group by 1
+having total_amount < 0
